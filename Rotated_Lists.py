538ff42917a9f39de88fd2e2868d165b0333316f
ghost237-sys <mwasczar03@gmail.com>
2023-10-21 21:36:18 +0300

    This is another binary seatch challenge

---
+++ Rotated_Lists.py
@@ -0,0 +1,7 @@
+"""
+    You are given a list of numbers, obtained by rotating a sorted list
+    an unknown number of times. write a function to determine the minimum number of times the original sorted
+    list was rotated to obtain the given list. Your function should have a worst-case complexity of O(log N), where
+    N is the length of the list.
+"""
+
